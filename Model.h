94f0bc5eb3ecf71d2fb972608c50f2515d059fc2
ozlamort <ozlamort@gmail.com>
2025-09-30 11:52:34 +0200

    :tada: Initial commit

---
+++ Model.h
@@ -0,0 +1,47 @@
+/**************Model.h**************/
+
+#ifndef MODEL_H
+#define MODEL_H
+
+
+#include <cstring>
+using namespace std;  // permet d'utiliser cout directement et non stdcout
+#include <iostream>
+
+enum Engine 
+{
+  Petrol, 
+  Diesel, 
+  Electric, 
+  Hybrid
+};
+
+class Model
+{
+  private:
+   char*name;
+   int power;
+   Engine engine;
+   float basePrice;
+
+  public:  // en dehors de la classe, ça doit être déclaré en public
+    Model();  // Constructeur
+    Model(const char*,int p,Engine e,float bp);
+    Model(const Model &m);
+    ~Model();   // Destructeur
+
+    void setName(const char*n);
+    void setPower(int p);
+    void setEngine(Engine e);
+    void setBasePrice(float bp);
+
+    const char* getName() const;
+    int getPower() const;
+    Engine getEngine() const;
+    float getBasePrice() const;
+
+    void display() const;
+
+};
+
+#endif
